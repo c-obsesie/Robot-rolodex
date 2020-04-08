0a27e3bbda47a2f82a2dd7ac89d077d0798274c2
c-obsesie <cosmin.mihalache95.com>
2020-04-08 23:46:34 +0100

    Updates

---
+++ precache-manifest.918b1263f46390bca5b170e45259eda7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f33ef86e28e36e570838d555e1cbf6e",
+    "url": "/robo-dex/index.html"
+  },
+  {
+    "revision": "6bf8a114272f45c754da",
+    "url": "/robo-dex/static/css/main.aa456815.chunk.css"
+  },
+  {
+    "revision": "4e13148e0247e6f402f5",
+    "url": "/robo-dex/static/js/2.c8fac5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-dex/static/js/2.c8fac5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf8a114272f45c754da",
+    "url": "/robo-dex/static/js/main.16135c9b.chunk.js"
+  },
+  {
+    "revision": "d32f08089b6d9c8b6300",
+    "url": "/robo-dex/static/js/runtime-main.fcc079e4.js"
+  }
+]);
